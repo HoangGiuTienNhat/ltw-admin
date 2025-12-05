4a247a4f438663cb6f1742af67868989bc970fe4
HoangGiuTienNhat <hgtnhat1@gmail.com>
2025-12-05 23:29:51 +0700

    fix svg admin anh Tan

--- dashboard/tri-admin/config.php
+++ dashboard/tri-admin/config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'techstore_db';
 $username = 'root';
-$password = '';
+$password = '123123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
